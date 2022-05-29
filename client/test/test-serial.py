fec8cfda8919a6b8097b2fc75070147ee0fb1d9c
JP Carrascal <juanpcarrascal@gmail.com>
2022-05-29 20:35:33 +0200

    Serial test script updated

--- client/test/test-serial.py
+++ client/test/test-serial.py
@@ -1,12 +1,18 @@
 import serial
 import time
+import sys
+
 ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
 ser.setDTR(False)
 #ser.reset_input_buffer()
 ser.flushInput()
 ser.setDTR(True)
 time.sleep(5)
-ser.write(b"02:02\n")
+if len(sys.argv) > 1:
+    command = bytes(sys.argv[1] + "\n")
+    ser.write(command)
+else:
+    ser.write(b"01:01\n")
 with ser:
     while True:
         print(ser.readline())
