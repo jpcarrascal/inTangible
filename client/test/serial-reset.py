f06e35dc7c1b9f901c59d81395556b7c7efaa30b
JP Carrascal <juanpcarrascal@gmail.com>
2022-05-30 09:21:11 +0200

    Trying to make motion test no-reset

--- client/test/serial-reset.py
+++ client/test/serial-reset.py
@@ -2,10 +2,10 @@ import serial
 import time
 
 ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
-ser.setDTR(False)
+ser.setDTR(True)
 time.sleep(2)
-ser.reset_input_buffer()
-ser.flushInput()
+#ser.reset_input_buffer()
+#ser.flushInput()
 #ser.setDTR(True)
 ser.setDTR(False)
 time.sleep(2)
